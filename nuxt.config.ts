61b8fc98562fe0b810c4fd9d97dcf03250b6ee74
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-03-20 15:26:13 +0800

    Add Firebase and VueFire integration with configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "@nuxtjs/color-mode",
     "@vueuse/nuxt",
+    "nuxt-vuefire",
   ],
 
   tailwindcss: {
@@ -36,4 +37,18 @@ export default defineNuxtConfig({
       },
     ],
   },
+
+  vuefire: {
+    auth:{ enabled: true, sessionCookie: true },
+    config: {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+      measurementId: process.env.measurementId,
+    },
+  },
+  
 });
