fc0498aa01cb8574066bbe653ee0f1fa0fae169f
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-03-20 15:37:05 +0800

    Configure TypeScript settings in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
@@ -22,6 +23,10 @@ export default defineNuxtConfig({
 
   colorMode: {
     classSuffix: "",
+  },
+
+  typescript: {
+    shim: false,
   },
 
   imports: {
