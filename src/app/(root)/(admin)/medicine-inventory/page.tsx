35b190e04df5c3ba451b31756feadc58be0629f1
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-04-05 22:52:02 +0800

    Add medicine inventory page for admin section

---
+++ src/app/(root)/(admin)/medicine-inventory/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+
+import Sidebar from "@/app/components/ui/admin-sidebar";
+
+const MedicineInventory = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      {/* Navbar */}
+      
+
+      {/* Sidebar & Main Content */}
+      <div className="flex flex-1">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Content Area */}
+        <main className="flex-1 p-6">
+          <h1 className="text-xl font-bold">This is the medicine inventory page</h1>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default MedicineInventory;
