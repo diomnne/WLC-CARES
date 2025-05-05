3952595e7e6aea8b8342710c99c725e7d6a0d18b
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-05-05 14:23:13 +0800

    Add dashboard component

--- src/app/(root)/(mih)/mih-dashboard/page.tsx
+++ src/app/(root)/(mih)/mih-dashboard/page.tsx
@@ -1,15 +1,15 @@
 "use client";
-import Sidebar from "@/app/components/ui/student-sidebar";
+//import Sidebar from "@/app/components/ui/mih-sidebar";
 
 const MIHDashboard = () => {
   return (
     <div className="flex flex-col h-screen">
       
       <div className="flex flex-1">
-        <Sidebar />
+        {/* <Sidebar /> */}
 
         <main className="flex-1 p-6">
-          <h1 className="text-xl font-bold">Medicine Inventory Handler Dashboard</h1>
+          <h1 className="text-xl font-bold pt-4">Medicine Inventory Handler Dashboard</h1>
         </main>
       </div>
     </div>
