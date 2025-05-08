d313344087b136c6ed6ea27c057d643f07f3311c
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-05-08 15:31:28 +0800

    Add doctor sidebar component and integrate into dashboard layout

--- src/app/(root)/(doctor)/doctor-dashboard/page.tsx
+++ src/app/(root)/(doctor)/doctor-dashboard/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-//import Sidebar from "@/app/components/ui/doctor-sidebar";
+import Sidebar from "@/app/components/ui/doctor-sidebar";
 
 const DoctorDashboard = () => {
   return (
     <div className="flex flex-col h-screen">
       
       <div className="flex flex-1">
-        {/* <Sidebar /> */}
+        <Sidebar />
 
         <main className="flex-1 p-6">
           <h1 className="text-xl font-bold pt-4">Doctor Dashboard</h1>
