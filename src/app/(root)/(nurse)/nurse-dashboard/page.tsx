6022aa700cd4961e2dfb079478b5d3dcf1298b5c
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-05-17 17:39:55 +0800

    Fix import statement for Sidebar component

--- src/app/(root)/(nurse)/nurse-dashboard/page.tsx
+++ src/app/(root)/(nurse)/nurse-dashboard/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-//import Sidebar from "@/app/components/ui/nurse-sidebar";
+import Sidebar from "@/app/components/sidebars/nurse-sidebar"
 
 const NurseDashboard = () => {
   return (
     <div className="flex flex-col h-screen">
       
       <div className="flex flex-1">
-        {/* <Sidebar /> */}
+        <Sidebar />
 
         <main className="flex-1 p-6">
           <h1 className="text-xl font-bold pt-4">Nurse Dashboard</h1>
